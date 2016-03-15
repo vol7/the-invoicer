8c7207f62d8bf6edb1d8eb57d2cb999ade4f17f9
Alexandre Buche <alexandre.buche@gmail.com>
2016-03-15 16:39:12 -0400

    State composed of only the invoice number

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
-import counter from './modules/counter'
+import invoice from './modules/invoice'
 
 export default combineReducers({
-  counter,
+  invoice,
   router
 })
