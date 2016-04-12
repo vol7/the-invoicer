ef27583620a64cb4abdd17aeb8e5e6213c2fa905
Alexandre Buche <alexandre.buche@gmail.com>
2016-04-12 11:09:49 -0400

    The items can now be ordered with drag and drop

--- src/components/form-item-list.js
+++ src/components/form-item-list.js
@@ -4,6 +4,9 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import update from 'react-addons-update'
 
+import { DragDropContext } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
+
 import FormItem from './form-item'
 
 import { invoiceActions } from '../redux/modules/invoice'
@@ -27,12 +30,30 @@ class FormItemList extends Component {
     this.addItem = this.addItem.bind(this)
     this.removeItem = this.removeItem.bind(this)
     this.changeItem = this.changeItem.bind(this)
+    this.moveItem = this.moveItem.bind(this)
+  }
+
+  moveItem (dragIndex, hoverIndex) {
+    const { items } = this.props.invoice
+    const dragItem = items[dragIndex]
+
+    const newState = update(this.props.invoice, {
+      items: {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, dragItem]
+        ]
+      }
+    })
+    this.props.fieldChange(newState)
   }
 
   changeItem (item, index) {
     const newState = update(this.props.invoice, {
       items: {
-        $splice: [[[index], 1, item]]
+        $splice: [
+          [[index], 1, item]
+        ]
       }
     })
     this.props.fieldChange(newState)
@@ -91,9 +112,4 @@ function mapDispatchToProps (dispatch) {
   moveItem: invoiceActions.moveItem}, dispatch)
 }
 
-export default (connect(mapStateToProps, mapDispatchToProps)(FormItemList))
-
-/*
-###############################################################################
-###############################################################################
-*/
+export default DragDropContext(HTML5Backend)(connect(mapStateToProps, mapDispatchToProps)(FormItemList))
--- src/components/form-item.js
+++ src/components/form-item.js
@@ -1,8 +1,79 @@
 import React from 'react'
-import { PropTypes } from 'react'
-import { Component } from 'react'
+import { PropTypes, Component } from 'react'
 import drag from '../../images/drag.svg'
 import update from 'react-addons-update'
+import { DragSource, DropTarget } from 'react-dnd'
+
+import { findDOMNode } from 'react-dom'
+
+const ItemTypes = {
+  ITEM: 'item'
+}
+
+const itemSource = {
+  beginDrag (props) {
+    return {
+      id: props.id,
+      index: props.index
+    }
+  }
+}
+
+const itemTarget = {
+  hover (props, monitor, component) {
+    const dragIndex = monitor.getItem().index
+    const hoverIndex = props.index
+
+    // Don't replace items with themselves
+    if (dragIndex === hoverIndex) {
+      return
+    }
+
+    // Determine rectangle on screen
+    const hoverBoundingRect = findDOMNode(component).getBoundingClientRect()
+
+    // Get vertical middle
+    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
+    // Determine mouse position
+    const clientOffset = monitor.getClientOffset()
+
+    // Get pixels to the top
+    const hoverClientY = clientOffset.y - hoverBoundingRect.top
+
+    // Only perform the move when the mouse has crossed half of the items height
+    // When dragging downwards, only move when the cursor is below 50%
+    // When dragging upwards, only move when the cursor is above 50%
+
+    // Dragging downwards
+    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+      return
+    }
+
+    // Dragging upwards
+    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+      return
+    }
+
+    // Time to actually perform the action
+    props.moveItem(dragIndex, hoverIndex)
+
+    monitor.getItem().index = hoverIndex
+  }
+}
+
+function dragCollect (connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  }
+}
+
+function dropCollect (connect) {
+  return {
+    connectDropTarget: connect.dropTarget()
+  }
+}
 
 class FormItem extends Component {
   static propTypes = {
@@ -10,7 +81,9 @@ class FormItem extends Component {
     changeItem: PropTypes.func.isRequired,
     index: PropTypes.number.isRequired,
     item: PropTypes.object.isRequired,
-    connectDragSource: PropTypes.func
+    connectDragSource: PropTypes.func,
+    connectDropTarget: PropTypes.func,
+    isDragging: PropTypes.bool.isRequired
   }
 
   constructor (props) {
@@ -32,8 +105,11 @@ class FormItem extends Component {
   }
 
   render () {
-    return (
-      <div>
+    const { connectDragSource, connectDropTarget, isDragging } = this.props
+    return connectDragSource(connectDropTarget(
+      <div style={{
+        opacity: isDragging ? 1 : 1
+      }}>
         <div>
           <div className='grid'>
             <div className='grid__col--10'>
@@ -85,8 +161,9 @@ class FormItem extends Component {
           <hr/>
         </div>
       </div>
-    )
+    ))
   }
 }
 
-export default (FormItem)
+export default DropTarget(ItemTypes.ITEM,
+ itemTarget, dropCollect)(DragSource(ItemTypes.ITEM, itemSource, dragCollect)(FormItem))
